8f6ba01c32703172a07b0c8dba81d61d8c635260
Laptop <Mustafaanis247@gmail.com>
2022-10-03 10:32:31 +0500

    Create some variable to get element by in question activity xml

--- app/src/main/java/com/example/myquizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/myquizapp/QuizQuestionActivity.kt
@@ -3,12 +3,44 @@ package com.example.myquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlin.math.log
 
 class QuizQuestionActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var inProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+
+        progressBar = findViewById(R.id.progress_bar)
+        inProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
+
         val questionsList = Constants.getQuestion()
         Log.i("QuestionsList size is ","${questionsList.size}")
+
+        for (i in questionsList){
+            log.e("Questions",i.question)
+        }
+
     }
 }
